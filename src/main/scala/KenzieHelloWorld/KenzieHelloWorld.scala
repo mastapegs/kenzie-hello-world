f5afe08cf0830af467705cfa18f8d83c02071d04
Matthew Pagan <mastapegs01@gmail.com>
2024-02-05 08:35:47 -0500

    kenzies helo world

--- src/main/scala/KenzieHelloWorld/KenzieHelloWorld.scala
+++ src/main/scala/KenzieHelloWorld/KenzieHelloWorld.scala
@@ -4,7 +4,9 @@ object KenzieHelloWorld {
   def main(args: Array[String]) = {
     println()
 
-    (1 to 100).foreach(_ => println("Hello, Kenzie!"))
+    List("kenzie", "noah", "nyah")
+      .map(name => s"Hello, ${name}")
+      .foreach(sentence => (1 to 4).foreach(_ => println(sentence)))
 
     println()
   }
